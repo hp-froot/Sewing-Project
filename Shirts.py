f27a02e41650a98de67f1e4f6986f61a2d974d70
Hannah <hpd1@yahoo.com>
2024-01-17 12:53:07 +0100

    Added new equations and comments for each equation in simpler terms as the variables are getting confusing

--- Shirts.py
+++ Shirts.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Shirt_measurements:
     def measurements(self, breast, ubl, shoulder, arm, hip, sl, slack, size):
         # defining measurements needed for shirts
@@ -74,12 +77,18 @@ class Simple_Shirt:
         self.shirt_length_back = self.shirt_measurements.sl - (
             ((1 / 2) * self.shirt_measurements.shoulder) + self.size_measurement_back()
         )
-        # length of the shirt front back
-        self.shirt_length_front_f = 
+        # length of the shirt front back - shoulder drop
+        self.shirt_length_front_f = (
+            self.shirt_measurements.sl - self.shoulder_measurements()
+        )
         # length of the shirt back back
         # front cleavage is a curve between the y-axis (1/2 shoulder + 1cm) and the x-axis (1/2 shoulder + cleavage size)
-        # shoulder strap front
-        self.width_front_up = self.shirt_measurements.shoulder - 1
+        # shoulder strap front top = shoulder - 1cm
+        self.width_front_up_top = self.shirt_measurements.shoulder - 1
+        # shoulder strap front drop = pythagoras (c = square root of a^2 + b^2)
+        self.width_front_up_drop = math.sqrt(
+            (self.width_front_up_top**2) + (self.shoulder_measurements() ** 2)
+        )
         # shoulder strap back
         self.width_back_up = (
             (1 / 4) * self.shirt_measurements.breast
